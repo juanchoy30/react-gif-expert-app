cc37a62c695d29d0b61ff3fdefb7ab58ee379206
Juan Miguel Choy <juanchoy30@gmail.com>
2021-01-26 17:20:05 -0300

    78.Custom Hook

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,20 +1,27 @@
-import React, { useState, useEffect } from 'react'
-import { getGif } from '../helpers/getGifs';
-import { GifGreedItem } from './GifGreedItem';
+import React  from 'react'
+import { useFetchGifs } from '../hooks/useFetchGifs';
+//import { getGif } from '../helpers/getGifs';
+//import { GifGreedItem } from './GifGreedItem';
 
 const GifGrid = ({ category }) => {
 
-    const [images, setImages] = useState([]);
+    
+    //const [images, setImages] = useState([]);
+    const { loading } = useFetchGifs();
 
+
+    /*
     useEffect( () => {
         getGif( category )
             .then( setImages );  // it is the same as .then( imgs => setImages( imgs ));
     }, [ category ]); // [] executes the function just once, if not the function could execute in an infinite cycle
-
+    */
     return (
         <>
             <h3>{ category }</h3>
-            <div className="card-grid">
+
+            { loading? 'Loading...' : 'Data Loaded' }
+            {/* <div className="card-grid">
                 {
                     images.map( img => (
                         <GifGreedItem 
@@ -23,7 +30,7 @@ const GifGrid = ({ category }) => {
                         />
                     ))
                 }
-            </div>
+            </div> */}
         </>
 
     )
