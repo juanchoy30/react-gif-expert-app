a6d0a99f9be9833ee30d0264a1da672af38531da
Juan Miguel Choy <juanchoy30@gmail.com>
2021-01-27 15:41:22 -0300

    93.Simulate changes on input

--- src/tests/components/AddCategory.test.js
+++ src/tests/components/AddCategory.test.js
@@ -5,13 +5,23 @@ import AddCategory from "../../components/AddCategory"
 describe('Component tests', () => {
 
     const setCategories = () => {};
+    const wrapper = shallow( <AddCategory setCategories={ setCategories }/> );
 
     test('should render correctly', () => {
         
-        const wrapper = shallow( <AddCategory setCategories={ setCategories }/> );
         expect( wrapper ).toMatchSnapshot();
 
+    });
 
-    })
+    test('should change the textbox', () => {
+        
+        const input = wrapper.find('input');
+        const value = 'Hello World'
+
+        input.simulate('change', { target: { value } });
+
+        expect( wrapper.find('p').text().trim() ).toBe( value )
+
+    });
     
 })
