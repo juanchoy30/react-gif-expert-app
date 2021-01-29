5ed533065565da5efa0efebcd58a118f7e12466e
Juan Miguel Choy <juanchoy30@gmail.com>
2021-01-29 11:03:54 -0300

    100.CustomHool waitForUpdate

--- src/tests/hooks/useFetchGifs.test.js
+++ src/tests/hooks/useFetchGifs.test.js
@@ -4,14 +4,29 @@ import { renderHook } from '@testing-library/react-hooks';
 
 describe('useFetchGifs hook tests', () => {
 
-    test('should return the initial state', () => {
+    test('should return the initial state', async() => {
         
-        const { result } = renderHook( () => useFetchGifs( 'The Office' ) );
+        const { result, waitForNextUpdate } = renderHook( () => useFetchGifs( 'The Office' ) );
         const { data, loading } = result.current;
+
+        await waitForNextUpdate();
 
         expect( data ).toEqual([]);
         expect( loading ).toBe(true);
 
+    });
+
+    test('should return an array of images and loading be false', async() => {
+        
+        const { result, waitForNextUpdate } = renderHook( () => useFetchGifs( 'The Office' ) );
+        await waitForNextUpdate();
+
+        const { data, loading } = result.current;
+
+        expect( data.length ).toBe(10);
+        expect( loading ).toBe(false);
+
     })
+    
     
 })
